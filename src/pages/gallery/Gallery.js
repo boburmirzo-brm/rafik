d04a4e897e67c032322b955bcf6120ca5723957a
Zokirkhon1002 <zokirxonkotibxanov@gmail.com>
2022-06-08 12:44:23 +0900

    gallery fixed

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,49 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import {db} from "../../server/firebase"
 import "./Gallery.css"
-// import dron from "../../assets/photos/dron.mp4"
-// import { gallery } from '../../static/data';
-// import { IoClose } from "react-icons/io5"
 import Footer from '../../components/footer/Footer';
 import Video from '../../components/video/Video';
-// import img10 from '../../assets/photos/4.jpg'
-// import img11 from '../../assets/photos/2.jpg'
-// import img12 from '../../assets/photos/3.jpg'
-// import img14 from '../../assets/photos/12.jpg'
-// import img15 from '../../assets/photos/15.jpg'
-// import movie from "../../assets/photos/dron.mp"
-// import movie1 from "../../assets/photos/building.mp4"
 import GallaryTwo from '../../components/galleryTwo/GallaryTwo';
 import LoaderImage from '../../components/loader/LoaderImage';
 
+// importing pictures and photos from assets folder
+import videoUrl from '../../assets/photos/video-portrait1.mp4'
+import videoUrl1 from '../../assets/photos/video-portrait.mp4'
+import videoUrl2 from '../../assets/photos/video2.mp4'
+import videoUrl3 from '../../assets/photos/building.mp4'
+import videoUrl4 from '../../assets/photos/video1.mp4'
+
+
+
+
 function Gallery() {
-    const [videoUrl, setVideoUrl] = useState(null)
-    const [videoUrlSecond, setVideoUrlSecond] = useState(null)
     const [imagesUrl, setImagesUrl] = useState(null)
 
-    useEffect(()=>{
-        db.collection("videos").onSnapshot(item=>(
-            setVideoUrl(item.docs.map(i=> i.data())[0].video )
-        ))
-    },[])
-    useEffect(()=>{
-        db.collection("videos").onSnapshot(item=>(
-            setVideoUrlSecond(item.docs.map(i=> i.data())[1].video )
-        ))
-    },[])
 
     useEffect(()=>{
         db.collection("images").onSnapshot(item=>(
             setImagesUrl(item.docs.map(i=> i.data()) )
         ))
     },[])
- 
+
+    console.log(videoUrl4)
   return <div id='galleryId' className='gallery'>
       <Video  name="Gallery" video={videoUrl}/>
         <div className="gallery_wrapper">
           <div className="gallery_wrapperTitle">
               <h1>Gallary</h1>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+              <p>Some photos of my dream America</p>
           </div>
           {
             imagesUrl? 
@@ -60,12 +49,12 @@ function Gallery() {
             </div>
             <div className="gallery_threeItem">
                 {
-                    videoUrl ? <video muted autoPlay loop="1000" src={videoUrl}></video> : <LoaderImage/>
+                    videoUrl1 ? <video muted autoPlay loop="1000" src={videoUrl1}></video> : <LoaderImage/>
                 }
             </div>
             <div className="gallery_threeItem">
                 {
-                    videoUrlSecond ? <video muted autoPlay loop="1000" src={videoUrlSecond}></video> : <LoaderImage/>
+                    videoUrl3 ? <video muted autoPlay loop="1000" src={videoUrl3}></video> : <LoaderImage/>
                 }
             </div>
         </div>
@@ -78,7 +67,7 @@ function Gallery() {
         <div className="gallery_three">
             <div className="gallery_threeItem">
                 {
-                    videoUrl ? <video muted autoPlay loop="1000" src={videoUrl}></video> : <LoaderImage/>
+                    videoUrl2 ? <video muted autoPlay loop="1000" src={videoUrl2}></video> : <LoaderImage/>
                 }
             </div>
             <div className="gallery_threeItem">
@@ -88,7 +77,7 @@ function Gallery() {
             </div>
             <div className="gallery_threeItem">
                 {
-                    videoUrlSecond ? <video muted autoPlay loop="1000" src={videoUrlSecond}></video> : <LoaderImage/>
+                    videoUrl4 ? <video muted autoPlay loop="1000" src={videoUrl4}></video> : <LoaderImage/>
                 }
             </div>
         </div>
